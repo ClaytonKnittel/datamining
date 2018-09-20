4af79a2132e453fd7b074484e75510455de8575f
Clayton Knittel <cknit1999@gmail.com>
2018-09-20 12:18:19 -0500

    Initial Commit

---
+++ datamining/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  datamining
+//
+//  Created by Clayton Knittel on 9/20/18.
+//  Copyright © 2018 Clayton Knittel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
